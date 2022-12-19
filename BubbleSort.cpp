f455b4b30c962c629e04ae29e0f5bc1cf16baae1
Prakriti Rawat <78833575+PrakritiRaw@users.noreply.github.com>
2022-12-19 23:36:15 +0530

    Create BubbleSort.cpp

---
+++ BubbleSort.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void BubbleSort(int arr[],int n)
+{for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-1-i;j++)
+        {
+            if(arr[j]>arr[j+1])
+              swap(arr[j],arr[j+1]);
+        }
+    }
+}
+int main()
+{  int arr[5]={4,2,6,1,7};
+   BubbleSort(arr,5);
+   for(int i=0;i<5;i++) cout<<arr[i]<<endl;
+}
